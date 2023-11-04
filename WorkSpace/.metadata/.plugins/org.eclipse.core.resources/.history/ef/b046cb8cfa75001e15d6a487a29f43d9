9dbb78773118b914190515a01a9481c21008ea2b
Cristhian Diaz <cristhiancamilodiazromero@gmail.com>
2023-11-04 18:19:23 -0500

    Borre algo

--- WorkSpace/.metadata/.plugins/org.eclipse.core.resources/.history/ef/b046cb8cfa75001e15d6a487a29f43d9
+++
@@ -1,5 +0,0 @@
-package co.edu.unbosque.model;
-
-public class ChanceDTO {
-
-}
--- WorkSpace/Diccionario/src/co/edu/unbosque/model/persistence/CRUDOperation.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.model.persistence;
-
-public interface CRUDOperation {
-
-	public void create(String... attrbs);
-
-	public String read();
-
-}
