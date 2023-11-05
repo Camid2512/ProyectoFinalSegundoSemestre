334b44202b0de2c0fbb0b16d6b89bc2fd1c94f59
Cristhian Diaz <cristhiancamilodiazromero@gmail.com>
2023-11-04 20:12:47 -0500

    Update con viewIncompeto

--- WorkSpaceGitHub/ProyectoFinalSegundoSemestre/src/co/edu/unbosque/model/persistence/HouseSettingDAO.java
+++ WorkSpaceGitHub/ProyectoFinalSegundoSemestre/src/co/edu/unbosque/model/persistence/HouseSettingDAO.java
@@ -1,5 +1,50 @@
 package co.edu.unbosque.model.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
 public class HouseSettingDAO {
+
+	private final String CONFIG_FILENAME;
+	private File myFile;
+	private PrintWriter fileWriter;
+
+	public HouseSettingDAO() {
+
+		CONFIG_FILENAME = "config.properties";
+
+	}
+
+	public void create(String name, int numberVenue, double totalBudget) {
+		myFile = new File("src/co/edu/unbosque/model/persistence/" + CONFIG_FILENAME);
+		myFile.delete();
+		try {
+			if (!myFile.exists()) {
+				myFile.createNewFile();
+				fileWriter = new PrintWriter(myFile);
+				fileWriter.write(
+						"PRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.nombrecasa="
+								+ name
+								+ "\nPRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.numerosede="
+								+ numberVenue
+								+ "\nPRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.presupuesto="
+								+ totalBudget);
+				fileWriter.close();
+			}
+			fileWriter = new PrintWriter(myFile);
+			fileWriter.write(
+					"PRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.nombrecasa="
+							+ name
+							+ "\nPRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.numerosede="
+							+ numberVenue
+							+ "\nPRF_PROG1_20232_DIAZCRISTHIAN_RUEDASANTIAGO_LARAVALENTINA_GUERREROCAMILO.porperties.presupuesto="
+							+ totalBudget);
+			fileWriter.close();
+		} catch (IOException e) {
+			System.out.println("Problema al crear el archivo, revise problemas de acceso.");
+		}
+	}
 
 }
---
+++ WorkSpaceGitHub/ProyectoFinalSegundoSemestre/src/co/edu/unbosque/view/BettingHouseManagmentWindow.java
@@ -0,0 +1,23 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class BettingHouseManagmentWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private JButton createBtn, exit, back;
+
+	private JTextField nameHouse, numberVenue, totalBudget;
+
+	public BettingHouseManagmentWindow() {
+		// TODO Auto-generated constructor stub
+
+	}
+
+}
